dc68f3c1dd7fff18f40ffd84eb3ab72e8463f3de
geff <zomg21@hotmail.com>
2018-11-20 23:44:21 +0000

    removed unused tile code

--- tile.h
+++ tile.h
@@ -23,46 +23,5 @@ namespace game
     int srcX;
     Collision collision;
   };
-  /*
-  class Tile
-  {
-
-
-
-  public:
-    enum Collision {
-      NONE = 0,
-      BOX = 1,
-      TOP_LEFT = 2,
-      TOP_RIGHT = 3,
-      BOTTOM_RIGHT = 4,
-      BOTTOM_LEFT = 5
-    };
-
-    int id;
-    std::string src;
-    int srcY;
-    int srcX;
-    Collision collision;
-
-    Tile();
-
-
-    Tile(
-      int id,
-      std::string src,
-      int srcX,
-      int srcY,
-      Tile::Collision collision
-    );
-    ~Tile();
-
-    int getID();
-    std::string getSrc();
-    int getSrcX();
-    int getSrcY();
-
-  };
-  */
 }
 #endif
