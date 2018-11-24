045021476c5f4292414fa349727126d9c68e495d
geff <zomg21@hotmail.com>
2018-11-24 12:59:46 +0000

    sprites are no longer blurry

--- oak/core/sprite.cpp
+++ oak/core/sprite.cpp
@@ -106,6 +106,7 @@ void Sprite::onDraw()
   
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, this->textureID);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glBindVertexArray(this->VAO);
 
   glm::mat4 model = glm::mat4(1.0);
