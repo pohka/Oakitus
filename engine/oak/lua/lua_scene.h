06877bb471497b603d752191ab4da3ab566388aa
geff <zomg21@hotmail.com>
2019-04-14 01:30:40 +0100

    start of json metadata validation

--- engine/oak/lua/lua_scene.h
+++ engine/oak/lua/lua_scene.h
@@ -5,9 +5,37 @@
 #include <string>
 #include <nlohmann/json.hpp>
 #include <oak/ecs/entity.h>
+#include <vector>
 
 namespace oak
 {
+  class JsonParam
+  {
+    public:
+      const char* key;
+      const char argType;
+      const bool isRequired;
+
+      JsonParam(const char* key, const char argType, const bool isRequired = true) :
+        key(key), argType(argType), isRequired(isRequired)
+      {
+
+      }
+  };
+
+  class JsonComp
+  {
+  public:
+    const char* name;
+    std::vector<JsonParam> params;
+
+    JsonComp(const char* name, const std::vector<JsonParam>& params) :
+      name(name), params(params)
+    {
+
+    }
+  };
+
   class LuaScene : public oak::Scene
   {
     static LuaScene* scene; //is this needed?, probably bad practice. scene manger already exposes the scene
@@ -16,12 +44,19 @@ namespace oak
     nlohmann::json data;
     bool dataLoaded = false;
 
+
   public:
 
     LuaScene(std::string path);
     void onLoad() override;
 
     nlohmann::json getPrefabData(const std::string& name);
+
+  private:
+
+
+    void setPrecacheFromData();
+    void validatePrefabData();
   };
 }
 
