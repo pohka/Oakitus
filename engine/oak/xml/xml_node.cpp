d0f749b7fceb5a12d3b94eac90cabd6f28ad8987
geff <zomg21@hotmail.com>
2019-04-26 15:39:08 +0100

    get and set functions for XMLNode

--- engine/oak/xml/xml_node.cpp
+++ engine/oak/xml/xml_node.cpp
@@ -17,3 +17,63 @@ XMLNode::~XMLNode()
   }
 }
 
+
+const std::map<std::string, std::string>& XMLNode::getAttrs() const
+{
+  return attrs;
+}
+
+std::string XMLNode::getAttr(const std::string& key) const
+{
+  auto it = attrs.find(key);
+  if (it != attrs.end())
+  {
+    return it->second;
+  }
+
+  return "";
+}
+
+bool XMLNode::hasAttr(const std::string& key) const
+{
+  auto it = attrs.find(key);
+  return it != attrs.end();
+}
+
+const std::string& XMLNode::getTag() const
+{
+  return tag;
+}
+
+const std::string& XMLNode::getTextContent() const
+{
+  return text;
+}
+
+void XMLNode::setTextContent(const std::string& text)
+{
+  this->text = text;
+}
+
+
+const std::vector<XMLNode*>& XMLNode::getChildNodes() const
+{
+  return children;
+}
+
+
+void XMLNode::addChildNode(XMLNode* node)
+{
+  node->parent = this;
+  children.push_back(node);
+}
+
+void XMLNode::setAttr(const std::string& key, const std::string& value)
+{
+  attrs[key] = value;
+}
+
+void XMLNode::setTag(const std::string& tag)
+{
+  this->tag = tag;
+}
--- engine/oak/xml/xml_node.h
+++ engine/oak/xml/xml_node.h
@@ -12,13 +12,49 @@ namespace oak
     XMLNode* parent;
     std::string tag;
     std::string text = "";
-    std::map<const std::string, std::string> attrs = {};
+    std::map<std::string, std::string> attrs = {};
     std::vector<XMLNode*> children;
 
   public:
     friend class XMLParser;
     XMLNode();
     ~XMLNode();
+
+
+    //Attributes
+    //------------------------------------
+    //get all attrs
+    const std::map<std::string, std::string>& getAttrs() const;
+    //get attribute value
+    std::string getAttr(const std::string& key) const;
+    //retruns true if attributes have a matching key
+    bool hasAttr(const std::string& key) const;
+    //set an attribute
+    void setAttr(const std::string& key, const std::string& value);
+
+
+    //Tag
+    //------------------------------------
+    //get tag
+    const std::string& getTag() const;
+    //set tag
+    void setTag(const std::string& tag);
+
+
+    //Text
+    //------------------------------------
+    //get text
+    const std::string& getTextContent() const;
+    //set text
+    void setTextContent(const std::string& text);
+
+    //Child nodes
+    //------------------------------------
+    //get child nodes
+    const std::vector<XMLNode*>& getChildNodes() const;
+
+    //add a child node to the end of children
+    void addChildNode(XMLNode* node);
   };
 }
 
